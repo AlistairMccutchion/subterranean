c08b32bfa5d344bc3863c9c36f6e7698f550b12f
Your Name <kalle@kallepersson.se>
2009-05-18 23:19:20 +0200

    Fixed screwup :)

---
+++ README.txt
@@ -0,0 +1,2 @@
+Created by the Subterranean Project
+Game engine created by (c) 2006 Phil Hassey / The Olde Battleaxe (http://www.imitationpickles.org/) 
---
+++ title.py
@@ -0,0 +1,42 @@
+import os
+
+import pygame
+from pygame.locals import *
+
+from pgu import engine
+
+from cnst import *
+
+class Title(engine.State):
+    def __init__(self,game):
+        self.game = game
+        
+    def init(self):
+        self.game.music('default')
+        self.bkgr = pygame.image.load(os.path.join("data","title","title.jpg")).convert()
+        self.frame = 0
+        
+        import rooms.sewer
+        self.next = rooms.sewer.Room(self.game,'title')
+
+        
+    def update(self,screen):
+        self.paint(screen)
+        
+    def paint(self,screen):
+		screen.blit(self.bkgr,(0,0))
+		pygame.display.flip()
+        
+    def event(self,e):
+        if e.type is KEYDOWN:
+            return self.next
+            
+        
+    def loop(self):
+        self.frame += 1
+        
+        if self.frame > (FPS*8):
+            return self.next
+
+        
+    
