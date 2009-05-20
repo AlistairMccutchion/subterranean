e925682d5cb5d4c1dfdeb7ab163ffb9188585a4c
Your Name <kalle@kallepersson.se>
2009-05-20 13:14:12 +0200

    Added a short into

--- title.py
+++ title.py
@@ -14,7 +14,11 @@ class Title(engine.State):
     def init(self):
         self.game.music('default')
         self.bkgr = pygame.image.load(os.path.join("data","title","title.png")).convert()
+        self.elevator = pygame.image.load(os.path.join("data","title","elevator.png")).convert_alpha()
+        self.darkness = pygame.image.load(os.path.join("data","title","darkness.png")).convert_alpha()
+        self.logo = pygame.image.load(os.path.join("data","title","logo.png")).convert_alpha()
         self.frame = 0
+        self.shake = 1
         
         import rooms.test
         self.next = rooms.test.Room(self.game,'title')
@@ -24,13 +28,25 @@ class Title(engine.State):
         self.paint(screen)
         
     def paint(self,screen):
-		screen.blit(self.bkgr,(0,0))
-		pygame.display.flip()
+        #KALLE: Me dont understand... :)
+        f = self.frame
+        n = FPS*3
+        i = f-FPS*3
+        y = SW*i/(FPS*1)
+        if i > int(FPS*4.5) and f%5 == 0:
+            screen.fill((0,0,0),(0,0,SW,485))
+        else:
+            if self.shake == 1: self.shake=2
+            else: self.shake = 1
+            screen.blit(self.bkgr,(self.shake,0))
+            screen.blit(self.elevator,(286,y))
+            screen.blit(self.darkness,(0,0))
+            screen.blit(self.logo,(25,175))
+        pygame.display.flip()
         
     def event(self,e):
         if e.type is KEYDOWN:
-            return self.next
-            
+            return self.next            
         
     def loop(self):
         self.frame += 1
