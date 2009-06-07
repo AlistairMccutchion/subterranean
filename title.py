64111e5919a1942e41cd34cdd63c04c2670b4205
Your Name <kalle@kallepersson.se>
2009-06-08 01:10:44 +0200

    Added a non-looping state, just as the game starts. Also changed the intro to a shorter one.

--- title.py
+++ title.py
@@ -18,16 +18,19 @@ class Title(engine.State):
         self.darkness = pygame.image.load(os.path.join("data","title","darkness.png")).convert_alpha()
         self.logo = pygame.image.load(os.path.join("data","title","logo.png")).convert_alpha()
         self.frame = 0
-        self.shake = 1
         
         import rooms.test
         self.next = rooms.test.Room(self.game,'title')
-
         
     def update(self,screen):
         self.paint(screen)
         
     def paint(self,screen):
+        screen.fill((0,0,0),(0,0,SW,SH))
+        screen.blit(self.logo,(195,200))
+        pygame.display.flip()
+        #Kalle: Old intro for reference
+        '''
         #KALLE: Me dont understand... :)
         f = self.frame
         n = FPS*3
@@ -36,13 +39,12 @@ class Title(engine.State):
         if i > int(FPS*4.5) and f%5 == 0:
             screen.fill((0,0,0),(0,0,SW,485))
         else:
-            if self.shake == 1: self.shake=2
-            else: self.shake = 1
-            screen.blit(self.bkgr,(self.shake,0))
+            screen.blit(self.bkgr,(0,0))
             screen.blit(self.elevator,(286,y))
             screen.blit(self.darkness,(0,0))
             screen.blit(self.logo,(25,175))
         pygame.display.flip()
+        '''
         
     def event(self,e):
         if e.type is KEYDOWN:
@@ -51,7 +53,7 @@ class Title(engine.State):
     def loop(self):
         self.frame += 1
         
-        if self.frame > (FPS*8):
+        if self.frame > (FPS/2):
             return self.next
 
         
